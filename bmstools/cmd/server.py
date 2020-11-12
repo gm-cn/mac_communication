bb1bb476f4824a381d4b39d480c09bd83ee14100
lizeen <zeen.li@capitalonline.net>
2020-11-12 17:50:42 +0800

    add auth

--- bmstools/cmd/server.py
+++ bmstools/cmd/server.py
@@ -13,9 +13,9 @@ def main():
     logger = log.setup()
     logger.info("start server")
     private_key, public_key = auth.gen_key()
-    with open("../pkg/server/private.pem", "w") as f:
+    with open(os.path.join(BASE_DIR, "/bmstools/pkg/server/private.pem"), "w") as f:
         f.write(private_key)
-    with open("../pkg/server/public.pem", "w") as f:
+    with open(os.path.join(BASE_DIR, "/bmstools/pkg/server/public.pem"), "w") as f:
         f.write(public_key)
 
     s = server.get_server()
