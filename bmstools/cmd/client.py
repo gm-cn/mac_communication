fa7dec6ffb3b269e84462f9eb4e74ae7c064c6b0
ming.geng <ming.geng@capitalonline.net>
2020-11-12 10:01:55 +0800

    add send_socket

--- bmstools/cmd/client.py
+++ bmstools/cmd/client.py
@@ -17,7 +17,7 @@ def main():
 
     c = client.get_client()
     c.start()
-    with c.new_session(dest_mac="\xb4\x96\x91\x2f\xcb\x40", src_mac="\xb4\x96\x91\x32\x31\xd8", vlan=1800) as session:
+    with c.new_session(dest_mac="\xb4\x96\x91\x32\x23\xa8", src_mac="\xb4\x96\x91\x33\x68\xd0", vlan=1708) as session:
         resp = session.exec_cmd("ls /root")
 
 
